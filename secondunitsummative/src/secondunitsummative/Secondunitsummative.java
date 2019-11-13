3cd2fb61dd2fe8eb88b80f40a8165daab91a9fa6
amabd7808 <amabd7808@7JGM6Y1.Student.UGDSB.ED>
2019-11-13 13:29:51 -0500

    my second push

--- secondunitsummative/src/secondunitsummative/Secondunitsummative.java
+++ secondunitsummative/src/secondunitsummative/Secondunitsummative.java
@@ -20,24 +20,8 @@ public class Secondunitsummative {
     public static void main(String[] args) {
      Scanner KeyedInput = new Scanner(System.in);
      int  level=0;
-     int markl1 ;
-     int markl1q1 ;
-     int markl1q2 ;
-     int markl1q3 ;
-     int markl2 ;
-     int markl2q1 ;
-     int markl2q2 ;
-     int markl2q3 ;
-     int markl2q4 ;
-     int markl3 ;
-     int markl3q1 ;
-     int markl3q2 ;
-     int markl3q3 ;
-     int markl3q4 ;
-     int markl3q5 ;
-     int percentl1;
-     int percentl2;
-     int percentl3 ;
+     int [ ] markl = new int [3]; 
+     int [ ] percentl = new int [3];  
      while (level != 4){
         System.out.println("________________anime quiz_____________");
         System.out.println("");
@@ -57,46 +41,49 @@ public class Secondunitsummative {
             System.out.println("Question 3: How many seasons is there in Tokyo ghoul");
             System.out.println("");
             System.out.println("write the answers of the three question down here↓↓↓");
+            System.out.print("Question 1 Ans:");
             level1q[0] = KeyedInput.nextInt();
+            System.out.print("Question 2 Ans:");
             level1q[1] = KeyedInput.nextInt();
+            System.out.print("Question 3 Ans:");
             level1q[2] = KeyedInput.nextInt();
-            
+          int [ ] markl1q = new int [3];
             if (level1q[0] == 909){
                 System.out.println("your Quistion 1 answer is correct");
-                markl1q1 = 1 ;
+                markl1q[0] = 1 ;
             }
             else {
                 System.out.println("your Quistion 1 answer is incorrect");
-                markl1q1 = 0 ;
+                markl1q[0] = 0 ;
             }
             if (level1q[1] == 3){
                 System.out.println("your Quistion 2 answer is correct");
-                markl1q2 = 1 ;
+                markl1q[1] = 1 ;
             }
             else {
                 System.out.println("your Quistion 2 answer is incorrect");
-                markl1q2 = 0 ;
+                markl1q[1] = 0 ;
             }
             if (level1q[2] == 2){
                 System.out.println("your Quistion 3 answer is correct");
-                markl1q3 = 1 ;
+                markl1q[2] = 1 ;
             }
             else {
                 System.out.println("your Quistion 3 answer is incorrect");
-                markl1q3 = 0 ;
+                markl1q[2] = 0 ;
             }
-            markl1 = markl1q1 + markl1q2 +markl1q3 ;
-            percentl1 = (markl1/3)*100 ;
-            if (markl1 == 3){
+            markl[0] = markl1q[0] + markl1q[1] +markl1q[2] ;
+            percentl[0] = (markl[0]/3)*100 ;
+            if (markl[0] == 3){
                 System.out.println("very good it is the best");
             }
-            else if (markl1 == 2){
+            else if (markl[0] == 2){
                 System.out.println("not bad but you can do better");
             }
-            else if (markl1 <= 1){
+            else if (markl[0] <= 1){
                 System.out.println("you kidding me ");
             }
-            System.out.println("you finl mark is "+markl1+"/3 which means"+Math.round(percentl1)+"");
+            System.out.println("you finl mark is "+markl[0]+"/3 which means"+Math.round(percentl[0])+"%");
         }
             
         else if (level == 2){
@@ -108,57 +95,62 @@ public class Secondunitsummative {
             System.out.println("Question 4: How many episodes is there in Naruto shippuden");
             System.out.println("");
             System.out.println("write the answers of the three question down here↓↓↓");
+            System.out.print("Question 1 Ans:");
             level2q[0] = KeyedInput.nextInt();
+            System.out.print("Question 2 Ans:");
             level2q[1] = KeyedInput.nextInt();
+            System.out.print("Question 3 Ans:");
             level2q[2] = KeyedInput.nextInt();
+            System.out.print("Question 4 Ans:");
             level2q[3] = KeyedInput.nextInt();
+            int [ ] markl2q = new int [4];  
             if (level2q[0] == 909){
                 System.out.println("your Quistion 1 answer is correct");
-                markl2q1 = 1 ;
+                markl2q[0] = 1 ;
             }
             else {
                 System.out.println("your Quistion 1 answer is incorrect");
-                markl2q1 = 0 ;
+                markl2q[0] = 0 ;
             }
             if (level2q[1] == 3){
                 System.out.println("your Quistion 2 answer is correct");
-                markl2q2 = 1 ;
+                markl2q[1] = 1 ;
             }
             else {
                 System.out.println("your Quistion 2 answer is incorrect");
-                markl2q2 = 0 ;
+                markl2q[1] = 0 ;
             }
             if (level2q[2] == 2){
                 System.out.println("your Quistion 3 answer is correct");
-                markl2q3 = 1 ;
+                markl2q[2] = 1 ;
             }
             else {
                 System.out.println("your Quistion 3 answer is incorrect");
-                markl2q3 = 0 ;
+                markl2q[2] = 0 ;
             }
             if (level2q[3] == 750){
                 System.out.println("your Quistion 4 answer is correct");
-                markl2q4 = 1 ;
+                markl2q[3] = 1 ;
             }
             else {
                 System.out.println("your Quistion 4 answer is incorrect");
-                markl2q4 = 0 ;
+                markl2q[3] = 0 ;
             }
-            markl2 = markl2q1 + markl2q2 +markl2q3 + markl2q4;
-            percentl2 = (markl2/4)*100 ;
-            if (markl2 == 4){
+            markl[1] = markl2q[0] + markl2q[1] +markl2q[2] + markl2q[3];
+            percentl[1] = (markl[1]/4)*100 ;
+            if (markl[1] == 4){
                 System.out.println("very good it is the best");
             }
-            else if (markl2 == 3){
+            else if (markl[1] == 3){
                 System.out.println("you can do better");
             }
-            else if (markl2 == 2){
+            else if (markl[1] == 2){
                 System.out.println("not bad ");
             }
-            else if (markl2 == 1){
+            else if (markl[1] == 1){
                 System.out.println("you kidding me  ");
             }
-            System.out.println("you finl mark is "+markl2+"/4 which means"+Math.round(percentl2)+"");
+            System.out.println("you finl mark is "+markl[1]+"/4 which means"+Math.round(percentl[1])+"%");
         }
         else if (level == 3){
             int [ ] level3q = new int [5]; 
@@ -170,80 +162,83 @@ public class Secondunitsummative {
             System.out.println("Question 5: at what episode did naruto marry");
             System.out.println("");
             System.out.println("write the answers of the three question down here↓↓↓");
+            System.out.print("Question 1 Ans:");
             level3q[0] = KeyedInput.nextInt();
+            System.out.print("Question 2 Ans:");
             level3q[1] = KeyedInput.nextInt();
+            System.out.print("Question 3 Ans:");
             level3q[2] = KeyedInput.nextInt();
+            System.out.print("Question 4 Ans:");
             level3q[3] = KeyedInput.nextInt();
+            System.out.print("Question 5 Ans:");
             level3q[4] = KeyedInput.nextInt();
+            int [ ] markl3q = new int [5]; 
             if (level3q[0] == 909){
                 System.out.println("your Quistion 1 answer is correct");
-                markl3q1 = 1 ;
+                markl3q[0] = 1 ;
             }
             else {
                 System.out.println("your Quistion 1 answer is incorrect");
-                markl3q1 = 0 ;
+                markl3q[0] = 0 ;
             }
             if (level3q[1] == 3){
                 System.out.println("your Quistion 2 answer is correct");
-                markl3q2 = 1 ;
+                markl3q[1] = 1 ;
             }
             else {
                 System.out.println("your Quistion 2 answer is incorrect");
-                markl3q2 = 0 ;
+                markl3q[1] = 0 ;
             }
             if (level3q[2] == 2){
                 System.out.println("your Quistion 3 answer is correct");
-                markl3q3 = 1 ;
+                markl3q[2] = 1 ;
             }
             else {
                 System.out.println("your Quistion 3 answer is incorrect");
-                markl3q3 = 0 ;
+                markl3q[2] = 0 ;
             }
             if (level3q[3] == 750){
                 System.out.println("your Quistion 4 answer is correct");
-                markl3q4 = 1 ;
+                markl3q[3] = 1 ;
             }
             else {
                 System.out.println("your Quistion 4 answer is incorrect");
-                markl3q4 = 0 ;
+                markl3q[3] = 0 ;
             }
             if (level3q[4] == 500){
                 System.out.println("your Quistion 4 answer is correct");
-                markl3q5 = 1 ;
+                markl3q[4] = 1 ;
             }
             else {
                 System.out.println("your Quistion 4 answer is incorrect");
-                markl3q5 = 0 ;
+                markl3q[4] = 0 ;
             }
-            markl3 = markl3q1 + markl3q2 +markl3q3 + markl3q4 + markl3q5 ;
-            percentl3 = (markl3/3)*100 ;
-            if (markl3 == 5){
+            markl[2] = markl3q[0] + markl3q[1] +markl3q[2] + markl3q[3] + markl3q[4] ;
+            percentl[2] = (markl[2]/3)*100 ;
+            if (markl[2] == 5){
                 System.out.println("very good it is the best");
             }
-            else if (markl3 == 4){
+            else if (markl[2] == 4){
                 System.out.println("you can do better");
             }
-            else if (markl3 == 3){
+            else if (markl[2] == 3){
                 System.out.println("not bad ");
             }
-            else if (markl3 == 2){
+            else if (markl[2] == 2){
                 System.out.println("you kidding me  ");
             }
-            else if (markl3 >= 1){
+            else if (markl[2] >= 1){
                 System.out.println("go back to grade 1  ");
             }
-            System.out.println("you finl mark is "+markl3+"/5 which means"+Math.round(percentl3)+"");
-            }
+            System.out.println("you finl mark is "+markl[2]+"/5 which means"+Math.round(percentl[2])+"%");
             }
             if (level == 4){
             System.out.println("byebye");
-            
-         
-          
-        }
+            }
      }
     }
-    
+}
+
      
 
 
